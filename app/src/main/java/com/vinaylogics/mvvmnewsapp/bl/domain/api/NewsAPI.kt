2736c7fae42a6a18de1d2fd9dcb983b71e24ca03
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-17 06:51:46 +0530

    Added the retrofit api and its implementation

--- app/src/main/java/com/vinaylogics/mvvmnewsapp/bl/domain/api/NewsAPI.kt
+++ app/src/main/java/com/vinaylogics/mvvmnewsapp/bl/domain/api/NewsAPI.kt
@@ -1,5 +1,8 @@
 package com.vinaylogics.mvvmnewsapp.bl.domain.api
 
+import com.vinaylogics.mvvmnewsapp.bl.domain.models.NewsResponse
+import com.vinaylogics.mvvmnewsapp.bl.domain.utils.Constants.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,7 +13,19 @@ interface NewsAPI {
         @Query("country")
         countryCode: String = "us",
         @Query("page")
-        pageNumber: Int =1
-    )
+        pageNumber: Int =1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String ,
+        @Query("page")
+        pageNumber: Int =1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
 
 }
---
+++ app/src/main/java/com/vinaylogics/mvvmnewsapp/bl/domain/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.vinaylogics.mvvmnewsapp.bl.domain.utils
+
+object Constants {
+
+    const val API_KEY = "a830fd214d8b45a3a65d48ced075049c"
+    const val BASE_URL = "https://newsapi.org"
+}
