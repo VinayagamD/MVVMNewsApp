916fbec08c9ba1180617a2660c5eb9ecc7658e44
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-17 08:14:55 +0530

    Added adapter for article display from the news along with the item view

---
+++ app/src/main/java/com/vinaylogics/mvvmnewsapp/ui/adapters/ArticleAdapter.kt
@@ -0,0 +1,59 @@
+package com.vinaylogics.mvvmnewsapp.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.vinaylogics.mvvmnewsapp.bl.domain.models.Article
+import com.vinaylogics.mvvmnewsapp.databinding.ItemArticlePreviewBinding
+
+class ArticleAdapter: RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder( val binding: ItemArticlePreviewBinding): RecyclerView.ViewHolder(binding.root)
+
+    private val differCallback = object: DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+    private val differ = AsyncListDiffer(this, differCallback)
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val binding = ItemArticlePreviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ArticleViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        with(differ.currentList[position]) {
+          Glide.with(holder.binding.root).load(urlToImage).into(holder.binding.ivArticleImage)
+          holder.binding.tvSource.text = source.name
+          holder.binding.tvTitle.text= title
+          holder.binding.tvDescription.text = description
+          holder.binding.tvPublishedAt.text = publishedAt
+           setOnItemClickListener {
+               onItemClickListener?.let { it(this) }
+           }
+
+        }
+    }
+
+
+}
